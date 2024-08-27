015546116aabda6c9d05e75e33b536491dc1f860
NotSamfa <samfalla31@gmail.com>
2024-08-27 18:56:10 -0400

    Add RestController

--- src/main/java/com/co/falla/verboHttp/controller/ItemController.java
+++ src/main/java/com/co/falla/verboHttp/controller/ItemController.java
@@ -7,67 +7,55 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import com.co.falla.verboHttp.model.GroceryItem;
 import com.co.falla.verboHttp.service.ItemService;
 
-@Controller
+@RestController
 public class ItemController {
 
 	@Autowired
 	private ItemService itemService;
 
 	@GetMapping("/getAll")
-	public ResponseEntity<List<GroceryItem>> getAll() {
-		List<GroceryItem> items = itemService.getAll();
-		return ResponseEntity.ok(items);
+	public List<GroceryItem> getAll() {
+        return itemService.getAll();
 	}
 
 
 	@PostMapping("/insert")
-	public ResponseEntity<GroceryItem> insert(@RequestBody GroceryItem groceryItem) {
-		GroceryItem savedItem = itemService.insert(groceryItem);
+	public GroceryItem insert(@RequestBody GroceryItem groceryItem) {
 
-		return ResponseEntity.ok(savedItem);
+        return itemService.insert(groceryItem);
 	}
 	
 	@PutMapping("/update")
-	public ResponseEntity<GroceryItem> update(@RequestBody GroceryItem groceryItem) {
-		GroceryItem updatedItem = itemService.update(groceryItem);
+	public GroceryItem update(@RequestBody GroceryItem groceryItem) {
 
-		return ResponseEntity.ok(updatedItem);
+        return itemService.update(groceryItem);
 	}
 	
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<String> updateData(@PathVariable String id) {
+	public String updateData(@PathVariable String id) {
 		boolean deleted = itemService.delete(id);
 
 		if (deleted) {
-			return ResponseEntity.ok("Item eliminado exitosamente");
+			return "Item eliminado exitosamente";
 		} else {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item no encontrado");
+			return "Item no encontrado";
 		}
 
 	}
 	
 	@PatchMapping("/updateData/{id}")
-	public ResponseEntity<GroceryItem>  updateData(@PathVariable String id, @RequestBody GroceryItem groceryItem) {
+	public GroceryItem  updateData(@PathVariable String id, @RequestBody GroceryItem groceryItem) {
 		GroceryItem updatedItem = itemService.updateData(id, groceryItem);
 
 		if (updatedItem != null) {
-			return ResponseEntity.ok(updatedItem);
+			return updatedItem;
 		} else {
-			return ResponseEntity.notFound().build();
+			return null;
 		}
 
 	}
